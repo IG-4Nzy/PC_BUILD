e071cbce55378b0b173e7ac6651b9d7eab40867b
athul <athulkrishnanas321@gmail.com>
2025-03-22 23:33:51 +0530

    .

--- pc-genie-builder-main/src/service/requests.ts
+++ pc-genie-builder-main/src/service/requests.ts
@@ -1,16 +1,26 @@
 import axios from "axios";
 const BASE_URL = `http://localhost:5104/`;
 
-export const adminLogin = async (body: {
-  userId: string;
-  password: string;
-}) => {
+export const adminLogin = async (body) => {
   try {
-    const res = await axios.post(`${BASE_URL}Admin/Login`, {
-      body
-    });
+    const res = await axios.post(
+      `${BASE_URL}Admin/Login`,
+      {
+        userId: body?.userId,
+        password: body?.password
+      },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
     console.log(res.data);
+    return res.data;
   } catch (error) {
     console.error("Error logging in:", error);
+    throw error;
   }
 };
